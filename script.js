45feee182959e86b70cd9c4be17dcd10936188f8
cwh2626 <cwh2626@gmail.com>
2020-07-15 20:40:22 +0900

    ADD style

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+let togglebtn = document.querySelector(".nav__togglebtn");
+let navbar = document.querySelector(".nav");
+let navMenu = document.querySelector(".nav__menu");
+let navEtc = document.querySelector(".nav__etc");
+togglebtn.onclick = function () {
+  navbar.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  navEtc.classList.toggle("active");
+};
